e8426a19808e9484bd3ef1d4523a1b777814914d
akiramiyamoto-dev <akiramiyamoto.dev@gmail.com>
2025-03-25 03:28:47 +0900

    novas configs2

--- Traducao_app/backend/src/server.ts
+++ Traducao_app/backend/src/server.ts
@@ -17,7 +17,7 @@ const app = express();
 // Middlewares globais
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173", // Substitua pelo domínio do frontend
+    origin: "https://estudo-js-eight.vercel.app/", // Substitua pelo domínio do frontend
     methods: ["GET", "POST", "PUT", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, //Permitir cookies e headers de autenticação
