d4e3d22c6a8f9a8c501b6630bc3983ccc13c69b9
akiramiyamoto-dev <akiramiyamoto.dev@gmail.com>
2025-03-25 01:48:05 +0900

    Railway e Vercel - Configurações

--- Traducao_app/frontend/src/main.tsx
+++ Traducao_app/frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 import App from './App.tsx'
@@ -7,7 +8,7 @@ import NavBar from './components/NavBar.tsx';
 import Footer from './components/Footer.tsx';
 import { AuthProvider } from "./context/AuthProvider.tsx";
 import HeroSection from './components/HeroSection.tsx';
-import { BrowserRouter } from 'react-router-dom';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
