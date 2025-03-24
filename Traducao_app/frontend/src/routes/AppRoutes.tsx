ccf57e8e797018cda382510320370c5871722807
akiramiyamoto-dev <akiramiyamoto.dev@gmail.com>
2025-03-25 01:21:31 +0900

    refatorando para deploy

--- Traducao_app/frontend/src/routes/AppRoutes.tsx
+++ Traducao_app/frontend/src/routes/AppRoutes.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "../pages/Login";
 import Dashboard from "../pages/Dashboard";
 import ClientArea from "../pages/ClientArea";
@@ -10,30 +10,30 @@ import Register from "../pages/Register";
 
 const AppRoutes: React.FC = () => {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route
-                    path="/dashboard"
-                    element={
-                        <PrivateRoute isAuthenticated={true} >
-                            <Dashboard />
-                        </PrivateRoute>
-                    }
-                />
-                <Route
-                    path="/client-area"
-                    element={
-                        <PrivateRoute isAuthenticated={true} >
-                            <ClientArea />
-                        </PrivateRoute>
-                    }
-                />
-                <Route path="/marriage-certificate" element={<MarriageCertificate />} />
-            </Routes>
-        </Router>
+
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route
+                path="/dashboard"
+                element={
+                    <PrivateRoute isAuthenticated={true} >
+                        <Dashboard />
+                    </PrivateRoute>
+                }
+            />
+            <Route
+                path="/client-area"
+                element={
+                    <PrivateRoute isAuthenticated={true} >
+                        <ClientArea />
+                    </PrivateRoute>
+                }
+            />
+            <Route path="/marriage-certificate" element={<MarriageCertificate />} />
+        </Routes>
+
     );
 };
 
