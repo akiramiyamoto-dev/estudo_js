bd00d434578abcd498dbd90e19b02bf014f5180f
Akira Miyamoto <akiramiyamoto.dev@gmail.com>
2025-03-25 21:19:19 +0900

    alterando estilos

--- Traducao_app/frontend/src/components/HeroSection.tsx
+++ Traducao_app/frontend/src/components/HeroSection.tsx
@@ -7,7 +7,7 @@ const HeroSection: React.FC = () => {
         < div className={`${styles.heroContainer} heroContainer`} style={{ height: 600 }} >
             <div className="row">
                 <div className="col-12">
-                    <div className={`${styles.hero} row Hero w-100`}>
+                    <div className={`${styles.hero} row Hero`}>
                         <h1 className="display-1 fw-bold pb-4">Traduções Profissionais</h1>
                         <p className=" lead"><strong>Oferecemos serviços de tradução com precisão e agilidade, <br />
                             garantindo que cada palavra seja fiel ao seu significado original.</strong></p>
